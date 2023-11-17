57d3392b5e7c26b965d41f89baff424aae33b42c
sk1930 <59496610+sk1930@users.noreply.github.com>
2023-11-17 15:07:57 -0500

    logging with log4j
    logging with log4j

--- 002pythonProject/HelloSpark.py
+++ 002pythonProject/HelloSpark.py
@@ -4,5 +4,21 @@ notes in 013 Creating Spark Project Build Configuration'''
 
 from pyspark.sql import *
 
+from lib.logger import Log4J
+
 if __name__ == "__main__":
     print("Starting hello spark")
+    spark = SparkSession.builder \
+    .appName("Hello Spark") \
+    .master("local[3]") \
+    .getOrCreate()
+    print("Starting hello spark2")
+
+    logger = Log4J(spark)
+    print("Starting hello spark3")
+
+    logger.info("Starting hello spark")
+    logger.info("Finished  hello spark")
+
+
+    spark.stop()
